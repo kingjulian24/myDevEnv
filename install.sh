b36d3bf51ccb39cc89079d9f675cb37029983d9f
Julian <brownjulian2@gmail.com>
2015-09-13 17:21:10 -0400

    init

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install avesome vim
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+
